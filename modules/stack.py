81bb9252733c5adb2981805d7b2691cbde6f0d9b
gy0p4k <gyorgypakozdi@gmail.com>
2017-10-08 16:57:15 +0200

    featstack): fetch first question from search by keywords

--- modules/stack.py
+++ modules/stack.py
@@ -1,8 +1,26 @@
 from modules import responder
+from stackapi import StackAPI
+
+
+SITE = StackAPI('stackoverflow')
+SITE.page_size = 1
+SITE.max_pages = 1
 
 def search(command, channel):
     keyword = get_search_keyword(command)
-    responder.echo(":stack_overflow: search to " + keyword, channel)
+    questions = SITE.fetch('questions', max=2, sort='votes', tagged=keyword)["items"]
+    answer = answer_search(questions)
+    if answer:
+        responder.echo(":stack_overflow: " + str(answer) , channel)
+    else:
+        responder.echo(":stack_overflow: Can't find anything :( " ,channel)
+
+def answer_search(questions):
+    if not len(questions) == 0:
+        return len(questions)
+    return
+
+
 
 def get_search_keyword(command):
-        return " ".join(command.split(" ")[1::])
+        return ";".join(command.split(" ")[1::])
