0b62d567fda7f3ec6da7bf0d260d09a573ad4c50
gy0p4k <gyorgypakozdi@gmail.com>
2017-10-08 15:27:56 +0200

    feat()

--- modules/fact.py
+++ modules/fact.py
@@ -4,6 +4,6 @@ from urllib import request
 source = "http://numbersapi.com/random/trivia"
 
 def get(command, channel):
-    responder.echo("Here is a fact:", channel)
+    responder.echo("There is a :mindblown:", channel)
     page = request.urlopen(source).read().decode('UTF-8')
     responder.echo(page, channel)
